273ccc4514530bc103d27572fed3d774e8c39c37
Prathyusha Acharya <124792319+prathyushaa5@users.noreply.github.com>
2024-12-22 08:52:55 +0530

    Update About.jsx

--- Documents/Portfolio-2/src/components/About.jsx
+++ Documents/Portfolio-2/src/components/About.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import aboutImg from "../assets/images/about.png";
+import aboutImg from "../assets/images/about.png"; // Assuming the image path is correct
+
 const About = () => {
-  const handleDownload = () => {
-    // Replace with the path to your CV file
-    const cvUrl = "../assets/Resume(1).pdf"
-    // Create a link element
-    const link = document.createElement('a');
-    link.href = cvUrl;
-    link.download = 'Resume(1).pdf'; // Optional: Set the default filename for the download
-    link.target = '_blank'; // Optional: Open in a new tab
-    
-    // Trigger the download
-    link.click();
-  };
+  const cvUrl = "/Resume(1).pdf"; // Place Resume(1).pdf in the public folder
+
   return (
     <section id="about" className="py-10 bg-black text-white">
       <div className="text-center mt-8">
@@ -23,17 +14,15 @@ const About = () => {
         <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
           <div className="p-2">
             <div className="text-gray-300 my-3">
-              <p className="text-justify leading-7  mx-auto">
-              I am Prathyusha, a 20-year-old engineering student from Surathkal, Mangalore. Currently, I am in my fourth year of studying Computer Science at St. Joseph Engineering College, Mangalore. Passionate about technology and problem-solving, I am eager to continue expanding my knowledge and skills in the field of computer science.
+              <p className="text-justify leading-7 mx-auto">
+                I am Prathyusha, a 20-year-old engineering student from Surathkal, Mangalore. Currently, I am in my fourth year of studying Computer Science at St. Joseph Engineering College, Mangalore. Passionate about technology and problem-solving, I am eager to continue expanding my knowledge and skills in the field of computer science.
               </p>
-            
               <br />
-              <br />
-             
-              <a  href={cvUrl} download> <button className="btn-primary">Download CV</button></a>
+              <a href={cvUrl} download>
+                <button className="btn-primary">Download CV</button>
+              </a>
             </div>
           </div>
-          
         </div>
       </div>
     </section>
